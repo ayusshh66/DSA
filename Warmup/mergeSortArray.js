03580e58cd6473812dfa4eeff8c8c586695910ba
ayusshh66 <adonisjefrey66@gmail.com>
2026-07-04 22:42:45 +0530

    merging array

---
+++ Warmup/mergeSortArray.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let copyNums1 = nums1.slice(0,m);
+    let n1 = 0;
+    let n2 = 0;
+    
+    for(i=0; i < m + n ; i++ ){
+        if(copyNums1[n1] < nums2[n2]){
+            nums1[i] = copyNums1[n1];
+            n1++;
+        }else{
+            nums1[i]  = nums2[n2];
+            n2++;
+        }
+    }
+};
