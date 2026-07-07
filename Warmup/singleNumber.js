aa5bfadcf3de22b9aee01b125cb679e3e1d32fc8
ayusshh66 <adonisjefrey66@gmail.com>
2026-07-07 12:39:52 +0530

    single Number from leetcode

---
+++ Warmup/singleNumber.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    
+    let hashMap = {};
+
+    for ( i = 0 ; i < nums.length ; i ++ ){
+        if (! hashMap[nums[i]] ){
+            hashMap[nums[i]] = 1 ;
+        } else {
+            hashMap[nums[i]] ++ ;
+        }
+    }
+
+    for ( i = 0 ; i < nums.length ; i ++ ){
+        if ( hashMap[nums[i]] === 1 ){
+            return nums[i] ;
+        }
+    } 
+};
