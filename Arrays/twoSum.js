86ad062a1eddb2289cf73b69dbd7ab1772ba3303
ayusshh66 <adonisjefrey66@gmail.com>
2026-07-08 16:41:57 +0530

    two sum hash map appraoch

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -1,14 +1,38 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var twoSum = function(nums, target) {
+//     for(let i =0 ; i<nums.length ; i++){
+//             for(let j = i+1; j <= (nums.length)-1; j++ ){
+//                 if(nums[i] + nums[j] === target){
+//                     return [i,j]
+//                 }
+//             }
+//         }
+// };
+
+// OPTIMAL APPROACH
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i =0 ; i<nums.length ; i++){
-            for(let j = i+1; j <= (nums.length)-1; j++ ){
-                if(nums[i] + nums[j] === target){
-                    return [i,j]
-                }
-            }
+    let hashMap = {};
+    let n = nums.length;
+
+    for(let i = 0 ; i < n ; i++){
+        hashMap[nums[i]] = i;
+    }
+
+    for(i=0; i <n ; i++){
+        let ans = target - nums[i];
+
+        if(hashMap[ans] && hashMap[ans] !== i){
+            return [i, hashMap[ans]]
         }
+    }
 };
