c7982c580a7dc7c38b1e4fea76d770c1a01430dd
ayusshh66 <adonisjefrey66@gmail.com>
2026-07-11 20:46:11 +0530

    longest consecutive sequence optimal approach

---
+++ Arrays/longestConsecutiveSequence.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    if(nums.length === 0) return 0
+        nums.sort((a,b) => a-b)
+
+        let count = 1;
+        let maxCount = 1;
+
+        for(let i = 0 ; i < nums.length - 1; i ++ ){
+
+            if(nums[i] === nums[i+1]){
+                continue;
+            }
+            if(nums[i+1] - nums[i] === 1){
+                count++
+            }else{
+                if(count > maxCount){
+                    maxCount = count;
+                }
+                count=1;
+            }
+        }
+    if(count> maxCount){
+        maxCount = count;
+    }
+    return maxCount;
+};
