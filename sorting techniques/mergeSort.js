d58920c23e19972e21d5563370f34d03a75287cc
ayusshh66 <adonisjefrey66@gmail.com>
2026-07-13 23:44:31 +0530

    merge sorting

--- sorting techniques/mergeSort.js
+++ sorting techniques/mergeSort.js
@@ -1,27 +1,63 @@
-const left = [9,5,4,1,10]
-const right = [0,7,4,6,1]
+// const left = [9,5,4,1,10]
+// const right = [0,7,4,6,1]
 
-function dsa(left,right){
+// function dsa(left,right){
 
-    let res = [];
+//     let res = [];
+//     let i = 0;
+//     let j = 0;
+
+//     while(i <  left.length && j < right.length){
+//         if(left[i]<right[j]){
+//             res.push(left[i])
+//             i++
+//         }else{
+//             res.push(right[j])
+//             j++
+//         }
+//     }
+
+//     return [...res,...left.slice(i),...right.slice(j)]
+
+
+// }
+
+// const result = dsa(left,right)
+
+// console.log(result);
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    
+    if(nums.length <= 0) return nums;
+
+    let mid = Math.floor(nums.length/2)
+    let left = sortArray(nums.slice(0,mid))
+    let right = sortArray(nums.slice(mid))
+
+    return merge(left,right)
+
+};
+
+function merge(left,right){
+
     let i = 0;
     let j = 0;
 
-    while(i <  left.length && j < right.length){
+    let res = [];
+
+    while(i < left.length && j < right.length){
         if(left[i]<right[j]){
-            res.push(left[i])
-            i++
+            res.push(left[i]);
+            i++;
         }else{
-            res.push(right[j])
+            res.push(right[j]);
             j++
         }
     }
-
-    return [...res,...left.slice(i),...right.slice(j)]
-
+    return [...res,...left,...right]
 
 }
-
-const result = dsa(left,right)
-
-console.log(result);
