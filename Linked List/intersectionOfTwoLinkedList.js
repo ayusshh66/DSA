ee671144d3394d8e1d84f9fab41da15ca1960a80
ayusshh66 <adonisjefrey66@gmail.com>
2026-07-22 22:57:34 +0530

    160. Intersection of Two Linked Lists

---
+++ Linked List/intersectionOfTwoLinkedList.js
@@ -0,0 +1,20 @@
+
+
+function intersection(headA, headB){
+
+    let set = new Set();
+
+    while(headA){
+        set.add(headA);
+        headA = headA.next;
+    }
+
+    while(headB){
+        if(set.has(headB)){
+            return headB
+        }
+        headB= headB.next
+    }
+    return null;
+
+}
