abe341f09bd4290a3f707fe3c34e76a490d1e180
ayusshh66 <adonisjefrey66@gmail.com>
2026-08-11 22:25:00 +0530

    cycle linked list fast and slow pointer appraoch

--- Neetcode250/linked list/cycleLinkedList.js
+++ Neetcode250/linked list/cycleLinkedList.js
@@ -2,17 +2,30 @@
 
 function cycle(head){
 
-    const hashMap = new Set();
+    // const hashMap = new Set();
 
-    while(head){
-        if(hashMap.has(head)){
+    // while(head){
+    //     if(hashMap.has(head)){
+    //         return true;
+    //     }else{
+    //         hashMap.add(head)
+    //         head = head.next;
+    //     }
+    // }
+
+    // return false;
+
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next;
+
+        if(fast===slow){
             return true;
-        }else{
-            hashMap.add(head)
-            head = head.next;
         }
     }
 
     return false;
-
 }
