33d9049515a30c3e0c09930a130470ea867596ed
ayusshh66 <adonisjefrey66@gmail.com>
2026-08-11 21:19:41 +0530

    linked list revision

---
+++ Neetcode250/linked list/reverseLinkedList.js
@@ -0,0 +1,21 @@
+
+
+function reverse(head){
+
+    let prev = null;
+    let curr = head;
+
+    while(curr){
+
+        let temp = curr.next;
+        curr.next = prev;
+
+        prev = curr;
+        curr = temp
+
+    }
+
+    return prev;
+
+
+}
