940e3c323f9ccfb05880a1e674b4db29eccb75b8
ayusshh66 <adonisjefrey66@gmail.com>
2026-08-19 23:43:09 +0530

    single loop approach length of last word leetcode

---
+++ strings/lengthOfLastWord2.js
@@ -0,0 +1,20 @@
+
+
+function lastWord(s){
+
+    let n = s.length -1 ;
+    let count = 0; 
+
+    while(n>=0){
+        if(s[n] !== " "){
+            count++
+        }else if(s[n] == " " && count>0){
+            break;
+        }
+
+        --n;
+    }
+
+    return count;
+
+}
