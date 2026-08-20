f9f9fb6dab9edfb81ba2040ca684fa5e07ac5e1d
ayusshh66 <adonisjefrey66@gmail.com>
2026-08-20 23:49:45 +0530

    find word containng leetcode strings

---
+++ strings/findWordContainig.js
@@ -0,0 +1,11 @@
+function find(words,x){
+    let res = [];
+
+    for(let i = 0 ; i < words.length ; i++){
+        if(words[i].includes(x)){
+            res.push(i);
+        }
+    }
+
+    return res;
+}
