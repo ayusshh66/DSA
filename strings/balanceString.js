da19e5d5cf4bc8c3a62151a8f075a94adf76e56f
ayusshh66 <adonisjefrey66@gmail.com>
2026-08-29 11:46:42 +0530

    balance string leetcode easy

---
+++ strings/balanceString.js
@@ -0,0 +1,26 @@
+function balance(s){
+
+    let temp = 0;
+    let count = 0;
+
+    for(let i=0; i < s.length; i++){
+
+        if(s[i]==="R"){
+            ++temp;
+        }else{
+            --temp;
+        }
+
+        if(temp === 0){
+            ++count;
+        }
+
+    }
+
+    return count;
+
+}
+
+const result = balance("RLRRLLRLRL")
+
+console.log(result);
